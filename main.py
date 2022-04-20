55cd182cc03ba075cd4fd66dbca0e15cff0231a1
rohit <rohit.chakraborty@enkindletech.com>
2022-04-20 13:09:14 +0530

    Update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from tkinter import messagebox
 
@@ -6,6 +7,10 @@ def main():
     task_name = "ACTA-Surveyor-Edition"
     command = "schtasks.exe /END /TN " + task_name
     subprocess.run(command)
+    py_ids = [int(line.split()[1]) for line in os.popen('tasklist').readlines()[3:] if line.split()[0] == "pythonw.exe"]
+    py_ids_to_kill = [id for id in py_ids if id != os.getpid()]
+    for pid in py_ids_to_kill:
+        os.system("taskkill /pid {} /T /F".format(pid))
     command = "schtasks.exe /RUN /TN " + task_name
     subprocess.run(command)
     messagebox.showinfo(title=task_name, message="Started successfully")
